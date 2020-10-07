6af2294a73869a2f0d9dffe503880948b649bb51
Plague <36628963+OFWModz@users.noreply.github.com>
2020-10-07 02:05:14 +0100

    Add files via upload

---
+++ Helpers.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Object = UnityEngine.Object;
+
+namespace PhasBright
+{
+    class Helpers
+    {
+        public static Player GetLocalPlayer()
+        {
+            List<Player> players = GetAllPlayers();
+
+            if (players == null || players.Count == 0)
+            {
+                return null;
+            }
+            else if (players.Count == 1)
+            {
+                return players[0];
+            }
+            else
+            {
+                foreach (Player player in players)
+                {
+                    if (player != null)
+                    {
+                        if (player.view != null)
+                        {
+                            if (player.view.isMine)
+                            {
+                                return player;
+                            }
+                        }
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        public static List<Player> GetAllPlayers()
+        {
+            Player[] players = Object.FindObjectsOfType<Player>();
+
+            if (players == null || players.Length == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return players.ToList();
+            }
+        }
+    }
+}
---
+++ PhasBrightByPlague.cs
@@ -0,0 +1,69 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace PhasBright
+{
+    public class PhasBrightByPlague : MelonMod
+    {
+        public static bool FullBright = false;
+
+        public override void OnGUI()
+        {
+            if (GUI.Toggle(new Rect(10, 10, 150, 25), FullBright, "Fullbright") != FullBright)
+            {
+                FullBright = !FullBright;
+
+                ToggleFullBright(FullBright);
+            }
+        }
+
+        public override void OnLevelWasLoaded(int level)
+        {
+            if (FullBright)
+            {
+                ToggleFullBright(FullBright);
+            }
+        }
+
+        public void ToggleFullBright(bool state)
+        {
+            Player LocalPlayer = Helpers.GetLocalPlayer();
+
+            if (LocalPlayer == null)
+            {
+                return;
+            }
+
+            var boneTransform = LocalPlayer.charAnim.GetBoneTransform(HumanBodyBones.Head);
+
+            if (boneTransform == null)
+            {
+                return;
+            }
+
+            var light = boneTransform.GetComponent<Light>();
+
+            if (state)
+            {
+                light = boneTransform.gameObject.AddComponent<Light>();
+
+                light.color = Color.white;
+                light.type = LightType.Spot;
+                light.shadows = LightShadows.None;
+                light.range = 99f;
+                light.spotAngle = 9999f;
+                light.intensity = 0.3f;
+            }
+            else
+            {
+                Object.Destroy(light);
+            }
+        }
+    }
+}
